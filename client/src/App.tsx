bd564c41e6a98c75d4d9ccf87a8b9adc3c9f4f4d
Jon Mico <jonmico@gmail.com>
2024-11-12 11:08:23 -0500

    Cleanup.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,97 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+interface User {
+  id: number;
+  email: string;
+}
+
+export default function App() {
+  const [error, setError] = useState('');
+  const [user, setUser] = useState<User | null>(null);
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
+    evt.preventDefault();
+
+    const user = { email, password };
+
+    try {
+      const res = await fetch('http://localhost:3000/user/register', {
+        method: 'POST',
+        body: JSON.stringify({ user }),
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      if (!res.ok) {
+        const errorData: { error: string } = await res.json();
+        setError(errorData.error);
+      }
+
+      const data: {
+        user: {
+          id: number;
+          email: string;
+        };
+      } = await res.json();
+
+      console.log(data);
+
+      setUser({ id: data.user.id, email: data.user.email });
+    } catch (err) {
+      if (err instanceof Error) {
+        setError(err.message);
+      } else {
+        setError('Something went wrong.');
+      }
+    }
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Yet Another Todo App</h1>
+      <form onSubmit={handleSubmit}>
+        <h2>Sign up</h2>
+        <div>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            value={email}
+            onChange={(evt) => setEmail(evt.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='password'
+            value={password}
+            onChange={(evt) => setPassword(evt.target.value)}
+          />
+        </div>
+        <button type='submit'>Sign up</button>
+      </form>
+      {user ? <User user={user} /> : null}
     </>
-  )
+  );
 }
 
-export default App
+interface UserProps {
+  user: User;
+}
+
+function User(props: UserProps) {
+  return (
+    <div>
+      <h3>{props.user.email}</h3>
+      <h4>{props.user.id}</h4>
+    </div>
+  );
+}
