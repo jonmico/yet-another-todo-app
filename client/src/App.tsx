0c1b74f6bc3e3c2fb8234b63f4bcfa6cd238c9eb
Jon Mico <jonmico@gmail.com>
2024-11-14 09:26:17 -0500

    Chop up App.tsx a little bit. Add react-router-dom and styled-components. Start early routing.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,97 +1,17 @@
-import { useState } from 'react';
-
-interface User {
-  id: number;
-  email: string;
-}
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import IndexPage from './pages';
+import RegisterPage from './pages/register-page';
 
 export default function App() {
-  const [error, setError] = useState('');
-  const [user, setUser] = useState<User | null>(null);
-
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
-    evt.preventDefault();
-
-    const user = { email, password };
-
-    try {
-      const res = await fetch('http://localhost:3000/user/register', {
-        method: 'POST',
-        body: JSON.stringify({ user }),
-        credentials: 'include',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      });
-
-      if (!res.ok) {
-        const errorData: { error: string } = await res.json();
-        setError(errorData.error);
-      }
-
-      const data: {
-        user: {
-          id: number;
-          email: string;
-        };
-      } = await res.json();
-
-      console.log(data);
-
-      setUser({ id: data.user.id, email: data.user.email });
-    } catch (err) {
-      if (err instanceof Error) {
-        setError(err.message);
-      } else {
-        setError('Something went wrong.');
-      }
-    }
-  }
-
   return (
     <>
-      <h1>Yet Another Todo App</h1>
-      <form onSubmit={handleSubmit}>
-        <h2>Sign up</h2>
-        <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={email}
-            onChange={(evt) => setEmail(evt.target.value)}
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={(evt) => setPassword(evt.target.value)}
-          />
-        </div>
-        <button type='submit'>Sign up</button>
-      </form>
-      {user ? <User user={user} /> : null}
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<IndexPage />}>
+            <Route path='register' element={<RegisterPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  );
-}
-
-interface UserProps {
-  user: User;
-}
-
-function User(props: UserProps) {
-  return (
-    <div>
-      <h3>{props.user.email}</h3>
-      <h4>{props.user.id}</h4>
-    </div>
   );
 }
---
+++ client/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import { Link, Outlet } from 'react-router-dom';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <h1>YATA</h1>
+      <h4>(Yet Another Todo App)</h4>
+      <Link to={'register'}>Register</Link>
+      <Outlet />
+    </div>
+  );
+}
