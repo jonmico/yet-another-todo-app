027b73eb7c036bb25045211811b6439c1d8c66db
Jon Mico <jonmico@gmail.com>
2024-11-27 10:05:16 -0500

    Start login controller.

--- server/src/controllers/user/login-controller.ts
+++ server/src/controllers/user/login-controller.ts
@@ -1,4 +1,12 @@
+import bcrypt from 'bcrypt';
 import { NextFunction, Request, Response } from 'express';
+import z from 'zod';
+import { db } from '../../db/db';
+
+const LoginSchema = z.object({
+  email: z.string(),
+  password: z.string(),
+});
 
 export async function loginController(
   req: Request,
@@ -6,7 +14,32 @@ export async function loginController(
   next: NextFunction
 ) {
   try {
-    console.log('NYI');
+    // handle this error
+    const { email, password } = LoginSchema.parse(req.body);
+
+    // handle this error
+    const user = await db.user.findUnique({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      res.status(400).json({ error: 'There is no user with that email.' });
+      return;
+    }
+
+    // hash/compare the plain text password
+    const isValidPassword = await bcrypt.compare(password, user.password);
+
+    if (!isValidPassword) {
+      res.status(403).json({ error: 'Invalid email or password.' });
+      return;
+    }
+    // issue access/refresh tokens
+    // send response back saying user is logged in
+    res.json({ message: 'hello!' });
+    return;
   } catch (err) {
     next(err);
   }
--- server/src/routes/user-routes.ts
+++ server/src/routes/user-routes.ts
@@ -1,7 +1,7 @@
 import { Router } from 'express';
-import { registerController } from '../controllers/user/register-controller';
-import { loginController } from '../controllers/user/login-controller';
 import { checkSessionController } from '../controllers/user/check-session-controller';
+import { loginController } from '../controllers/user/login-controller';
+import { registerController } from '../controllers/user/register-controller';
 import { verifyTokens } from '../middleware/verify-tokens';
 
 export const userRouter = Router();
