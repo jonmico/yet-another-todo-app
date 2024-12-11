f7f7d67fd531e58deb0290730fe1ab8bd713ac43
Jon Mico <jonmico@gmail.com>
2024-12-11 09:02:21 -0500

    Switch RegisterPage over to useActionState.

--- client/src/pages/register-page.tsx
+++ client/src/pages/register-page.tsx
@@ -1,55 +1,78 @@
-import { useState } from 'react';
+import { useActionState } from 'react';
 import { useAuth } from '../hooks/useAuth';
 import { registerApi } from '../services/auth-api/register-api';
 
-export default function RegisterPage() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const { authState, authDispatch } = useAuth();
+interface RegisterFormState {
+  email: string;
+  password: string;
+  confirmPassword: string;
+  errors: {
+    emailError?: string;
+    passwordError?: string;
+    confirmPasswordError?: string;
+  };
+}
 
-  async function handleSubmit(evt: React.FormEvent<HTMLFormElement>) {
-    evt.preventDefault();
+const initalState: RegisterFormState = {
+  email: '',
+  password: '',
+  confirmPassword: '',
+  errors: {},
+};
+
+export default function RegisterPage() {
+  const { authState, authDispatch } = useAuth();
+  const [formState, formAction, isPending] = useActionState<
+    RegisterFormState,
+    FormData
+  >(async (_, formData: FormData) => {
+    const email = formData.get('email') as string;
+    const password = formData.get('password') as string;
 
     const data = await registerApi(email, password);
 
-    authDispatch({
-      type: 'auth/createUser',
-      payload: { id: data.user.id },
-    });
-  }
+    authDispatch({ type: 'auth/createUser', payload: { id: data.user.id } });
+
+    return { email: '', password: '', confirmPassword: '', errors: {} };
+  }, initalState);
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <h2>Sign up</h2>
+    <>
+      {isPending ? (
+        <div>PENDING...</div>
+      ) : (
         <div>
-          <label htmlFor='email'>Email</label>
-          <input
-            type='email'
-            name='email'
-            id='email'
-            value={email}
-            onChange={(evt) => setEmail(evt.target.value)}
-          />
+          <form action={formAction}>
+            <h2>Sign up</h2>
+            <div>
+              <label htmlFor='email'>Email</label>
+              <input
+                type='email'
+                name='email'
+                id='email'
+                defaultValue={formState.email}
+              />
+            </div>
+            <div>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                name='password'
+                id='password'
+                defaultValue={formState.password}
+              />
+            </div>
+            <button type='submit'>Sign up</button>
+          </form>
+          {authState.id ? <User id={authState.id} /> : <div>Not logged in</div>}
         </div>
-        <div>
-          <label htmlFor='password'>Password</label>
-          <input
-            type='password'
-            name='password'
-            id='password'
-            value={password}
-            onChange={(evt) => setPassword(evt.target.value)}
-          />
-        </div>
-        <button type='submit'>Sign up</button>
-      </form>
-      {authState.id ? <User id={authState.id} /> : null}
-    </div>
+      )}
+    </>
   );
 }
 
 interface UserProps {
-  id: number;
+  id: string;
 }
 
 function User({ id }: UserProps) {
