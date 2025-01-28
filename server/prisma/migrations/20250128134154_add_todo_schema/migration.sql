bf3d7bd354b372af12f810f80af1a65825537adf
Jon Mico <jonmico@gmail.com>
2025-01-28 08:44:42 -0500

    Add Todo schema.

---
+++ server/prisma/migrations/20250128134154_add_todo_schema/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE `Todo` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NOT NULL,
+    `description` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Todo_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Todo` ADD CONSTRAINT `Todo_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
