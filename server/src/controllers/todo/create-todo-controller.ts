aa3d8752aed26e6138a11ebd433f8ef60dbe2c19
Jon Mico <jonmico@gmail.com>
2025-01-30 09:48:54 -0500

    We can now make a todo.

--- server/src/controllers/todo/create-todo-controller.ts
+++ server/src/controllers/todo/create-todo-controller.ts
@@ -1,21 +1,47 @@
 import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
+import { db } from '../../db/db';
 
 // TODO: Probably add some type of minimum length onto these
 const CreateTodoSchema = z.object({
   title: z.string(),
   description: z.string(),
+  userId: z.string(),
 });
 
-// TODO: Build this.
+// TODO: Make return better.
+// TODO: Error handling.
 export async function createTodoController(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
-  const result = CreateTodoSchema.safeParse(req.body);
+  try {
+    const result = CreateTodoSchema.safeParse(req.body);
 
-  console.log(result);
+    if (!result.data) {
+      const errors = result.error.flatten().fieldErrors;
 
-  res.json(result);
+      res.status(400).json({
+        error: {
+          title: errors.title?.join(', '),
+          description: errors.description?.join(', '),
+          userId: errors.userId?.join(', '),
+        },
+      });
+      return;
+    }
+
+    const todo = await db.todo.create({
+      data: {
+        title: result.data.title,
+        description: result.data.description,
+        userId: result.data.userId,
+      },
+    });
+
+    res.json(todo);
+  } catch (err) {
+    next(err);
+  }
 }
