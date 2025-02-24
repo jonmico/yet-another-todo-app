8253cefd00ce97e6397c1a7378121f4323f203ae
Jon Mico <jonmico@gmail.com>
2025-02-24 11:35:46 -0500

    Add redirect on home page so user goes to dashboard if they are logged in.

--- client/app/routes/home-page.tsx
+++ client/app/routes/home-page.tsx
@@ -1,3 +1,15 @@
+import { sessionCookie } from '~/sessions.server';
+import type { Route } from './+types/home-page';
+import { redirect } from 'react-router';
+
+export async function loader({ request }: Route.LoaderArgs) {
+  const session = await sessionCookie.getSession(request.headers.get('Cookie'));
+
+  if (session.get('userId')) {
+    return redirect('/dashboard');
+  }
+}
+
 export default function HomePage() {
-  return <div>this is the home page!</div>;
+  return <div>Welcome to Yet Another Todo App</div>;
 }
