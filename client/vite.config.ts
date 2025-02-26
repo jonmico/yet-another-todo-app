757d4f107d6ec0cd8fdbb7c7905f699346562593
Jon Mico <jonmico@gmail.com>
2025-02-26 12:20:53 -0500

    Install @tailwindcss/vite so tailwind works.

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,16 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite';
-import autoprefixer from 'autoprefixer';
-import tailwindcss from 'tailwindcss';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [tailwindcss, autoprefixer],
-    },
-  },
-  plugins: [reactRouter(), tsconfigPaths()],
+  // css: {
+  //   postcss: {
+  //     plugins: [autoprefixer],
+  //   },
+  // },
+  plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
