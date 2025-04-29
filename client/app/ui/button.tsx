79cbc786a4751856ba52b87ffe4e2a8f1e5a4b15
Jon Mico <jonmico@gmail.com>
2025-04-29 09:44:29 -0400

    Fix button FIXME. Add default styles for button cursor. I am team pointer.

--- client/app/ui/button.tsx
+++ client/app/ui/button.tsx
@@ -6,17 +6,13 @@ interface ButtonProps {
   loadingState?: 'idle' | 'loading' | 'submitting';
 }
 
-/*
-FIXME: Loading spinner shows on navigation, which is unintended. We do not want to show a loading spinner when we are navigating
-between pages, only when a form is submitted. Possible fix: only show loading spinner on 'submitting'?
-*/
 export default function Button({ type, children, loadingState }: ButtonProps) {
   return (
     <button
       className='background-color flex items-center justify-center gap-2 rounded-sm bg-blue-600 p-2 font-semibold transition delay-150 ease-in-out hover:bg-blue-700'
       type={type}
     >
-      {loadingState === 'loading' && <Spinner />} {children}
+      {loadingState === 'submitting' && <Spinner />} {children}
     </button>
   );
 }
