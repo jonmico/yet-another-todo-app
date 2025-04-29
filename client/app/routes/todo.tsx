22e6ae02825f8b6f6b77630f9916834bb69f0a3a
Jon Mico <jonmico@gmail.com>
2025-04-29 10:26:52 -0400

    Style TODO a little bit. Maybe break Todo out into its own component sometime.

--- client/app/routes/todo.tsx
+++ client/app/routes/todo.tsx
@@ -5,6 +5,7 @@ import type { Route } from './+types/todo';
 import { Link, useFetcher, useLoaderData } from 'react-router';
 import FormError from '~/ui/form-error';
 import type { action } from './delete-todo';
+import PageHeader from '~/components/page-header';
 
 // TODO: Style Todo component.
 
@@ -30,18 +31,22 @@ export default function TodoPage({ loaderData }: Route.ComponentProps) {
       {fetcher.data && <FormError message={fetcher.data.error._server} />}
       {loaderData.error && <div>Error: {loaderData.error._server}</div>}
       <Todo />
-      <Link
-        to={'edit'}
-        className='rounded border px-4 py-1'
-      >
-        Edit
-      </Link>
-      <fetcher.Form
-        method='delete'
-        action='delete'
-      >
-        <button className='rounded border p-4'>Delete</button>
-      </fetcher.Form>
+      <div className='flex gap-2'>
+        <Link
+          to={'edit'}
+          className='rounded border border-yellow-500 bg-yellow-700/75 px-4 py-1 text-slate-200'
+        >
+          Edit
+        </Link>
+        <fetcher.Form
+          method='delete'
+          action='delete'
+        >
+          <button className='rounded border border-red-500 bg-red-700/75 px-4 py-1 text-slate-300'>
+            Delete
+          </button>
+        </fetcher.Form>
+      </div>
     </div>
   );
 }
@@ -54,10 +59,34 @@ function Todo() {
   if (!todo) return <div>{loaderData.error._server}</div>;
 
   return (
-    <div>
-      {todo.title && <div>Title: {todo.title}</div>}
-      <div>Description: {todo.description}</div>
-      {todo.dueDate && <div>Due Date: {todo.dueDate}</div>}
+    <div className='flex flex-col gap-2'>
+      <TodoTitle title={todo.title} />
+      <div className='rounded bg-slate-900 p-3'>
+        <div>{todo.description}</div>
+        <TodoDueDate dueDate={todo.dueDate} />
+      </div>
     </div>
   );
+}
+
+interface TodoTitleProps {
+  title: string | undefined;
+}
+
+function TodoTitle(props: TodoTitleProps) {
+  if (!props.title) return null;
+
+  return <PageHeader>{props.title}</PageHeader>;
+}
+
+interface TodoDueDateProps {
+  dueDate: string | undefined;
+}
+
+function TodoDueDate(props: TodoDueDateProps) {
+  if (!props.dueDate) return null;
+
+  const formattedDate = new Date(props.dueDate).toDateString();
+
+  return <div>{formattedDate}</div>;
 }
